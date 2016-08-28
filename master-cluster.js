7872e6a068de9d335b5ad62d20e83870ba3e268e
Jerome Touffe-Blin <jtblin@gmail.com>
2016-08-28 15:41:33 +1000

    Cosmetic changes

--- master-cluster.js
+++ master-cluster.js
@@ -17,7 +17,9 @@ function start (options) {
 
   cluster.setupMaster(options);
 
-  var reload = options.reload || options.reload === false ? options.reload : /^dev/.test(process.env.NODE_ENV), counter = 0;
+  var counter = 0;
+  var reload = options.reload || options.reload === false ? options.reload : /^dev/.test(process.env.NODE_ENV);
+
   if (reload ) {
     reloader.reload(options);
     cluster.reset = function () {
@@ -29,6 +31,7 @@ function start (options) {
   eachCluster(options.size, cluster.fork.bind(cluster));
 
   cluster.on('disconnect', function (worker) {
+    debug('Worker %d with pid %s disconnected', worker.id, worker.process.pid);
     if (! reload) {
       cluster.fork();
       return;
@@ -71,7 +74,8 @@ function createHttpServer (handler, port, onShutdown) {
 }
 
 function setFnHandlers (runFn, errorFn) {
-  setup.run = runFn, setup.error = errorFn || function () {};
+  setup.run = runFn;
+  setup.error = errorFn || function () {};
   return this;
 }
 
@@ -84,15 +88,18 @@ function onWorkerError (err) {
       process.exit(1)
     }, 30000);
     // But don't keep the process open just for that!
-    if (typeof killtimer.unref === 'function') killtimer.unref();
+    if (typeof killtimer.unref === 'function')
+      killtimer.unref();
 
     // Let the master know we're dead.  This will trigger a
     // 'disconnect' in the cluster master, and then it will fork
     // a new worker.
-    if (cluster.worker && !cluster.worker.suicide) cluster.worker.disconnect();
+    if (cluster.worker && !(cluster.worker.exitedAfterDisconnect || cluster.worker.suicide))
+      cluster.worker.disconnect();
 
     // stop everything
-    if (typeof setup.error === 'function') setup.error(err);
+    if (typeof setup.error === 'function')
+      setup.error(err);
 
   } catch (er2) {
     // oh well, not much we can do at this point.
